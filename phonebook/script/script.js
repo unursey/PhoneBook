120cbc34780b590d07a84dffe94d17434a709372
Anna O <usune@mail.ru>
2022-07-03 23:29:03 +0300

    modul05lesson07

--- phonebook/script/script.js
+++ phonebook/script/script.js
@@ -24,6 +24,11 @@ const data = [
 ];
 
 {
+  const addContactData = contact => {
+    data.push(contact);
+    console.log(data);
+  };
+
   const createContainer = () => {
     const container = document.createElement('div');
     container.classList.add('container');
@@ -193,12 +198,12 @@ const data = [
       },
     ]);
     const table = createTable();
-    const form = createForm();
+    const {form, overlay} = createForm();
     const footer = createFooter();
 
     header.headerContainer.append(logo);
     footer.footerContainer.append(copyright);
-    main.mainContainer.append(buttonGroup.btnWrapper, table, form.overlay);
+    main.mainContainer.append(buttonGroup.btnWrapper, table, overlay);
     app.append(header, main, footer);
 
     return {
@@ -206,8 +211,8 @@ const data = [
       logo,
       btnAdd: buttonGroup.btns[0],
       btnDel: buttonGroup.btns[1],
-      formOverlay: form.overlay,
-      form: form.form,
+      formOverlay: overlay,
+      form,
     };
   };
 
@@ -264,18 +269,78 @@ const data = [
     });
   };
 
+  const modalControl = (btnAdd, formOverlay) => {
+    const openModal = () => {
+      formOverlay.classList.add('is-visible');
+    };
+
+    const closeModal = () => {
+      formOverlay.classList.remove('is-visible');
+    };
+
+    btnAdd.addEventListener('click', openModal);
+
+    formOverlay.addEventListener('click', (e) => {
+      const target = e.target;
+      if (target === formOverlay || target.classList.contains('close')) {
+        closeModal();
+      }
+    });
+
+    return {
+      closeModal,
+    };
+  };
+
+  const deleteControl = (btnDel, list) => {
+    btnDel.addEventListener('click', () => {
+      document.querySelectorAll('.delete').forEach((del) => {
+        del.classList.toggle('is-visible');
+      });
+    });
+
+    list.addEventListener('click', (e) => {
+      if (e.target.closest('.del-icon')) {
+        e.target.closest('.contact').remove();
+      }
+    });
+  };
+
+  const addContactPage = (contact, list) => {
+    list.append(createRow(contact));
+  };
+
+  const formControl = (form, list, closeModal) => {
+    form.addEventListener('submit', e => {
+      e.preventDefault();
+      const formData = new FormData(e.target);
+      const newContact = Object.fromEntries(formData);
+
+      addContactPage(newContact, list);
+      addContactData(newContact);
+      form.reset();
+      closeModal();
+    });
+  };
+
   const init = (selectorApp, title) => {
     const app = document.querySelector(selectorApp);
-    const phoneBook = renderPhoneBook(app, title);
 
-    const { list, logo, btnAdd, formOverlay, form, btnDel } = phoneBook;
+    const {
+      list,
+      logo,
+      btnAdd,
+      formOverlay,
+      form,
+      btnDel,
+    } = renderPhoneBook(app, title);
 
     const allRow = renderContacts(list, data);
-    hoverRow(allRow, logo);
+    const {closeModal} = modalControl(btnAdd, formOverlay);
 
-    btnAdd.addEventListener('click', () => {
-      formOverlay.classList.add('is-visible');
-    });
+    hoverRow(allRow, logo);
+    deleteControl(btnDel, list);
+    formControl(form, list, closeModal);
 
     // form.addEventListener('click', event => {
     //   event.stopPropagation();
@@ -301,25 +366,6 @@ const data = [
     //   }
     // };
     // formOverlay.addEventListener("click", toggleForm);
-
-    formOverlay.addEventListener('click', (e) => {
-      const target = e.target;
-      if (target === formOverlay || target.classList.contains('close')) {
-        formOverlay.classList.remove('is-visible');
-      }
-    });
-
-    btnDel.addEventListener('click', () => {
-      document.querySelectorAll('.delete').forEach((del) => {
-        del.classList.toggle('is-visible');
-      });
-    });
-
-    list.addEventListener('click', (e) => {
-      if (e.target.closest('.del-icon')) {
-        e.target.closest('.contact').remove();
-      }
-    });
 
     const sortRows = function(index) {
       const rows = document.querySelectorAll('.contact');
